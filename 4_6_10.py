6ac09098edd8e1323870f3eec8fc5f5974810e11
gbrs <gbrs@mail.ru>
2021-10-19 18:21:41 +0300

    solved 4.6.10 spiral filling of the matrix

--- 4_6_10.py
+++ 4_6_10.py
@@ -2,10 +2,15 @@
 заполнение спиралью матрицы n на m
 """
 
-n, m = 2, 1
+
+# генерация исходного списка
+n, m = [int(i) for i in input().split()]
 lst = [[None] * m for _ in range(n)]
-# print(lst)
-cnt = 1109
+
+# счетчик, который будет давать нам числа, которыми мы заполняем спираль
+cnt = 1 - 2
+
+# маркеры, показывающие на границу незаполненного участка
 top = 0
 bottom = n
 left = -1
@@ -13,44 +18,50 @@ right = m
 
 
 while True:
+    # заполняем матрицу по спирали, начиная с верхней строки
 
+    # сперва подтягиваем дальнюю границу к себе
     right -= 1
-    if left <= right:
+    # если границы не совпадают, то заполняем строчку/столбик
+    if left < right:
         for i in range(left, right):
             cnt += 1
             lst[top][i] = cnt
-        [print(*row) for row in lst]
-        print()
+    # если же граница совпала, то заполняем последний элемент
+    # и брекаем всё
     else:
+        cnt += 1
+        lst[top][right] = cnt
         break
 
     bottom -= 1
-    if top <= bottom:
+    if top < bottom:
         for i in range(top, bottom):
             cnt += 1
             lst[i][right] = cnt
-        [print(*row) for row in lst]
-        print()
     else:
+        cnt += 1
+        lst[bottom][right] = cnt
         break
 
     left += 1
-    if left <= right:
+    if left < right:
         for i in range(right, left, -1):
             cnt += 1
             lst[bottom][i] = cnt
-        [print(*row) for row in lst]
-        print()
     else:
+        cnt += 1
+        lst[bottom][left] = cnt
         break
 
     top += 1
-    if top <= bottom:
+    if top < bottom:
         for i in range(bottom, top, -1):
             cnt += 1
             lst[i][left] = cnt
-        [print(*row) for row in lst]
-        print()
     else:
+        cnt += 1
+        lst[top][left] = cnt
         break
 
+[print(*row) for row in lst]
--- Probe.py
+++ Probe.py
@@ -1,2 +1,61 @@
-for i in range(0):
-    print(i)
+n, m = 2, 3
+lst = [[None] * m for _ in range(n)]
+# print(lst)
+cnt = 1109
+top = 0
+bottom = n
+left = -1
+right = m
+
+
+while True:
+
+    right -= 1
+    if left < right:
+        for i in range(left, right):
+            cnt += 1
+            lst[top][i] = cnt
+        [print(*row) for row in lst]
+        print()
+    else:
+        cnt += 1
+        lst[top][right] = cnt
+        break
+
+    bottom -= 1
+    if top < bottom:
+        for i in range(top, bottom):
+            cnt += 1
+            lst[i][right] = cnt
+        [print(*row) for row in lst]
+        print()
+    else:
+        cnt += 1
+        lst[bottom][right] = cnt
+        break
+
+    left += 1
+    if left < right:
+        for i in range(right, left, -1):
+            cnt += 1
+            lst[bottom][i] = cnt
+        [print(*row) for row in lst]
+        print()
+    else:
+        cnt += 1
+        lst[bottom][left] = cnt
+        break
+
+    top += 1
+    if top < bottom:
+        for i in range(bottom, top, -1):
+            cnt += 1
+            lst[i][left] = cnt
+        [print(*row) for row in lst]
+        print()
+    else:
+        cnt += 1
+        lst[top][left] = cnt
+        break
+
+[print(*row) for row in lst]
