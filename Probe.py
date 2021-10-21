861b08d5169d3546a8b00360c21324b5d8e8550b
gbrs <gbrs@mail.ru>
2021-10-21 21:31:34 +0300

    added notes.txt

--- Probe.py
+++ Probe.py
@@ -1,61 +1,7 @@
-n, m = 2, 3
-lst = [[None] * m for _ in range(n)]
-# print(lst)
-cnt = 1109
-top = 0
-bottom = n
-left = -1
-right = m
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+print(a | b)  # {1, 2, 3, 4, 5, 6}
+print(a & b)  # {3, 4}
+print(a - b)  # {1, 2}
+print(a ^ b)  # {1, 2, 5, 6}
 
-
-while True:
-
-    right -= 1
-    if left < right:
-        for i in range(left, right):
-            cnt += 1
-            lst[top][i] = cnt
-        [print(*row) for row in lst]
-        print()
-    else:
-        cnt += 1
-        lst[top][right] = cnt
-        break
-
-    bottom -= 1
-    if top < bottom:
-        for i in range(top, bottom):
-            cnt += 1
-            lst[i][right] = cnt
-        [print(*row) for row in lst]
-        print()
-    else:
-        cnt += 1
-        lst[bottom][right] = cnt
-        break
-
-    left += 1
-    if left < right:
-        for i in range(right, left, -1):
-            cnt += 1
-            lst[bottom][i] = cnt
-        [print(*row) for row in lst]
-        print()
-    else:
-        cnt += 1
-        lst[bottom][left] = cnt
-        break
-
-    top += 1
-    if top < bottom:
-        for i in range(bottom, top, -1):
-            cnt += 1
-            lst[i][left] = cnt
-        [print(*row) for row in lst]
-        print()
-    else:
-        cnt += 1
-        lst[top][left] = cnt
-        break
-
-[print(*row) for row in lst]
