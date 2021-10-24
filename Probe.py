7fa662772c74525a7825f5240fe56689ec838ed0
gbrs <gbrs@mail.ru>
2021-10-24 11:11:25 +0300

    solved 8.7.5 set operations

--- Probe.py
+++ Probe.py
@@ -1,7 +1,8 @@
-a = {1, 2, 3, 4}
-b = {3, 4, 5, 6}
-print(a | b)  # {1, 2, 3, 4, 5, 6}
-print(a & b)  # {3, 4}
-print(a - b)  # {1, 2}
-print(a ^ b)  # {1, 2, 5, 6}
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Spain', 'Cameroon')
+number = countries.count('Spain')
+cnt = 0
+for country in countries:
+    if country == 'Spain':
+        cnt += 1
+
 
