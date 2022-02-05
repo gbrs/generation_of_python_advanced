a9a13f275a25c364abf776a18ebcea82fa113d14
gbrs <gbrs@mail.ru>
2022-02-05 12:33:03 +0300

    solved 12.3.1

---
+++ 12_3_1.py
@@ -0,0 +1,14 @@
+import random
+
+n = 10 ** 6
+
+cnt_in_figure = 0
+for _ in range(n):
+    x = random.uniform(-2, 2)
+    y = random.uniform(-2, 2)
+    if (-2 <= x <= 2 and
+            -2 <= y <= 2 and
+            x ** 3 + y ** 4 + 2 >= 0 and
+            3 * x + y ** 2 <= 2):
+        cnt_in_figure += 1
+print(cnt_in_figure / n * 16)
